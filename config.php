19b141752a7feb8027e87149c97777028eac909f
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-05 21:53:40 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,13 @@
 <?php 
-
- $host = "localhost:manny.db.elephantsql.com (manny-01)";
+ $host = "manny.db.elephantsql.com (manny-01)";
  $user = "iyrbjpkv";
  $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
- $database = "iyrbjpkv";
-//$db=mysqli_connect($host,$user,$pass,$database) or die("database not connected");
-$db=pg_connect("host=$host dbname=$database user=$user password=$pass")
+ $dbs = "iyrbjpkv";
+
+ // Open a PostgreSQL connection
+ $db = mysqli_connect("host=$host dbname=$dbs user=$user password=$pass")
    or die ("Could not connect to server\n");
+
+//$db=mysqli_connect($host,$user,$pass,$database) or die("database not connected");
+
 ?>
