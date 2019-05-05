4c8b15d47e87d5c07599b84b76e0f4de52a9797a
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-05 21:36:41 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
  $host = "manny.db.elephantsql.com (manny-01)";
  $user = "iyrbjpkv";
  $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
- $db = "iyrbjpkv";
-$db=mysqli_connect($host,$user,$pass,$db) or die("database not connected");
+ $database = "iyrbjpkv";
+$db=mysqli_connect($host,$user,$pass,$database) or die("database not connected");
 ?>
