68ef5a7a211a6c8e7f66876aa62732eb6fa3186a
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-05 21:40:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,5 +4,7 @@
  $user = "iyrbjpkv";
  $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
  $database = "iyrbjpkv";
-$db=mysqli_connect($host,$user,$pass,$database) or die("database not connected");
+//$db=mysqli_connect($host,$user,$pass,$database) or die("database not connected");
+$db=pg_connect("host=$host dbname=$database user=$user password=$pass")
+   or die ("Could not connect to server\n");
 ?>
