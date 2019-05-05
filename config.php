6f30b20ae845b954bcb2d53cbf4efaa4b732ca71
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-05 21:23:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php 
-$db=mysqli_connect("localhost","root","","physio") or die("database not connected");
+$db=mysqli_connect("manny.db.elephantsql.com (manny-01)","iyrbjpkv","IFFB3unX25uJPCj9u9PojgTg7CAOexon","iyrbjpkv") or die("database not connected");
 ?>
