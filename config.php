6e85fa5f8fb8c6beedd59583b9b4aa437031ac4e
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-06 15:09:16 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,19 +6,15 @@
  // $db = mysqli_connect('manny.db.elephantsql.com (manny-01)','iyrbjpkv','IFFB3unX25uJPCj9u9PojgTg7CAOexon','iyrbjpkv')
  //or die('Error connecting to MySQL server.');
 
- $host = "	manny.db.elephantsql.com (manny-01)";
+ $host = "manny.db.elephantsql.com (manny-01)";
  $user = "iyrbjpkv";
- $pass = "	IFFB3unX25uJPCj9u9PojgTg7CAOexon";
+ $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
  $db = "iyrbjpkv";
 
  // Open a PostgreSQL connection
- $con = pg_connect("host=$host dbname=$db user=$user password=$pass")
+ $con =pg_connect("host=$host dbname=$db user=$user password=$pass")
    or die ("Could not connect to server\n");
 
- $query = 'SELECT * FROM admin';
- $results = pg_query($con, $query) or die('Query failed: ' . pg_last_error());
-
- $row = pg_fetch_row($results);
  echo $row[0] . "\n";
  // Closing connection
  pg_close($con);
