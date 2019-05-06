555b13d15cbb5025515dce0c76af56ea4b0c7e67
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-06 10:23:52 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,11 @@
 <?php 
- //$host = "localhost:manny.db.elephantsql.com (manny-01):5432";
- //$user = "iyrbjpkv";
- //$pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
- //$dbs = "iyrbjpkv";
+ $host = "localhost:manny.db.elephantsql.com (manny-01):5432";
+ $user = "iyrbjpkv";
+ $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
+ $dbs = "iyrbjpkv";
  
+$db=pg_connect("host=$host dbname=$db user=$user password=$pass")
+   or die ("Could not connect to server\n");
 
  // Open a PostgreSQL connection
 // $db = mysqli_connect("host=$host dbname=$dbs user=$user password=$pass")
@@ -11,12 +13,11 @@
 
 //$db=mysqli_connect($host,$user,$pass,$dbs) or die("database not connected");
 //$db=mysql_connect('manny.db.elephantsql.com (manny-01):5432','iyrbjpkv','IFFB3unX25uJPCj9u9PojgTg7CAOexon') or die("conncection Error...!");
-$servername = "	postgres://iyrbjpkv:IFFB3unX25uJPCj9u9PojgTg7CAOexon@manny.db.elephantsql.com:5432/iyrbjpkv";
-$username = "iyrbjpkv";
-$password = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
-
+//$servername = "	postgres://iyrbjpkv:IFFB3unX25uJPCj9u9PojgTg7CAOexon@manny.db.elephantsql.com:5432/iyrbjpkv";
+//$username = "iyrbjpkv";
+//$password = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
 // Create connection
-$db = new mysqli_connect($servername, $username, $password);
+//$db = new mysqli($servername, $username, $password);
 
 
 ?>
