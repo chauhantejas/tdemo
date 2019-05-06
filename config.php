8c49196410e828be8745a168cb387641f85dbef0
chauhantejas <48180794+chauhantejas@users.noreply.github.com>
2019-05-06 08:38:29 +0530

    changes port

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php 
- $host = "manny.db.elephantsql.com (manny-01):5432";
+ $host = "manny.db.elephantsql.com (manny-01)";
  $user = "iyrbjpkv";
  $pass = "IFFB3unX25uJPCj9u9PojgTg7CAOexon";
  $dbs = "iyrbjpkv";
+$port="5432";
 
  // Open a PostgreSQL connection
 // $db = mysqli_connect("host=$host dbname=$dbs user=$user password=$pass")
   // or die ("Could not connect to server\n");
 
-$db=mysql_connect($host,$user,$pass,$dbs) or die("database not connected");
+$db=mysqli_connect($host,$user,$pass,$dbs,$port) or die("database not connected");
 
 ?>
